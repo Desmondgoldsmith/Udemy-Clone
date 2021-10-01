9e8524ddf0d8a2732749fcad155cb8d4874a3557
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2021-10-01 00:44:05 -0700

    New Commit 😆😆💀💀

--- courses/models.py
+++ courses/models.py
@@ -1,36 +1,45 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 import uuid
 # Create your models here.
-class courses(models.Model):
+User = get_user_model()
+
+class Sector(models.Model):
+    name = models.CharField(max_length=225)
+    sector = models.UUIDField(default=uuid.uuid4, unique= True)
+    related_courses = models.ManyToManyField('Course')
+    sector_image = models.ImageField(upload_to = 'sector_image')
+
+class Course(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add = True)
     updated= models.DateTimeField(auto_now = True)
-    # author = models.ForeignKey(user)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,)
     language = models.CharField(max_length=60)
-    course_section = models.ManyToManyField('course_section')
+    course_section = models.ManyToManyField('CourseSection')
     comments = models.ManyToManyField('comments')
     images = models.ImageField(upload_to = 'course_images')
     course_uuid = models.UUIDField(default=uuid.uuid4,unique= True)
     price = models.DecimalField(max_digits=5,decimal_places=2)
 
 
-class courseSection(models.Model):
+class CourseSection(models.Model):
     title = models.CharField(max_length=255)
-    episodes = models.ManyToManyField('episodes') 
+    episodes = models.ManyToManyField('Episodes') 
 
-class episodes(models.Model):
+class Episodes(models.Model):
     title = models.CharField(max_length=225)
     files = models.FileField(upload_to = 'course_videos')
     length= models.DecimalField(max_digits=10,decimal_places=2)
 
-class comments(models.Model):
-    # user =  user = models.ForeignKey(user)
+class Comments(models.Model):
+    user_ =  models.ForeignKey(User,on_delete=models.CASCADE)
     message = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
 
-class sector(models.Model):
-     name= models.CharField(max_length=225)
-     sector_uuid = models.UUIDField(default=uuid.uuid4, unique=True)
-     related_courses = models.ManyToManyField('Courses')
-     sector_image = models.ImageField(upload_to = 'sector_image')
+# class sector(models.Model):
+#      name= models.CharField(max_length=225)
+#      sector_uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+#      related_courses = models.ManyToManyField('Courses')
+#      sector_image = models.ImageField(upload_to = 'sector_image')
--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_Superuser(self,email,password,name,**other_fields):
+        other_fields.setdefault('is_staff',True)
+        other_fields.setdefault('is_superuser',True)
+
+        if other_fields.get('is_staff') is not True:
+            return ValueError('superuser must be staff')
+
+        if other_fields.get('is_superuser') is not True:
+            return ValueError('superuser must be superuser')    
+
+        return self.createuser(email,password,name,**other_fields)
+
+    def createuser(self,email,password,name,**other_fields):
+       if not email:
+           raise ValueError('You should Provide A Valid Email.')
+       email = self.normalize_email(email) 
+       user = self.model(email=email,name=name,**other_fields) 
+
+       user.set_password(password) 
